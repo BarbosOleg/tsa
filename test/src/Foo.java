ded928648d76819f157fc1abeb70ca0587456438
BarbosOleg <lolipo.celio@gmail.com>
2021-02-02 00:33:43 +0200

    commit

---
+++ test/src/Foo.java
@@ -0,0 +1,25 @@
+public class Foo {
+    private int first;
+    private int second;
+
+    public Foo(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+}
