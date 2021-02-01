579e903470ef1bd121d29ad3d6fc30a4cd143cee
BarbosOleg <lolipo.celio@gmail.com>
2021-02-02 00:38:43 +0200

    add bigger

--- test/src/Foo.java
+++ test/src/Foo.java
@@ -22,4 +22,11 @@ public class Foo {
     public void setFirst(int first) {
         this.first = first;
     }
+    public int bigger(){
+        if (this.first > this.second)
+            return this.first;
+        else if(this.second == this.first)
+            return second;
+        else return this.second;
+    }
 }
